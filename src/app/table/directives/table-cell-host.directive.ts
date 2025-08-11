f6a6eca30fca1075e314bca75d5b7d9c2b400f53
Eyal Mendel <eyalmendel@gmail.com>
2025-08-11 09:41:06 +0300

    logic

--- src/app/table/directives/table-cell-host.directive.ts
+++ src/app/table/directives/table-cell-host.directive.ts
@@ -5,6 +5,6 @@ import { Directive, ViewContainerRef } from "@angular/core";
 })
 export class TableCellHostDirective {
 
-    constructor(viewContainerRef: ViewContainerRef) {}
+    constructor(public viewContainerRef: ViewContainerRef) {}
 
 }
--- src/app/table/table.ts
+++ src/app/table/table.ts
@@ -1,5 +1,6 @@
-import { Component, input } from '@angular/core';
-import {CdkTableModule} from '@angular/cdk/table';
+import { CdkTableModule } from '@angular/cdk/table';
+import { AfterViewInit, Component, input, QueryList, ViewChildren } from '@angular/core';
+import { TableCellHostDirective } from './directives/table-cell-host.directive';
 import { TableColumn } from './interfaces/table-column.interface';
 import { TableColumnRendererRegistryService } from './services/table-column-renderer-registry.service';
 
@@ -9,7 +10,7 @@ import { TableColumnRendererRegistryService } from './services/table-column-rend
   templateUrl: './table.html',
   styleUrl: './table.css'
 })
-export class Table {
+export class Table implements AfterViewInit {
 
     data = input<Object[]>([]);
 
@@ -17,8 +18,32 @@ export class Table {
 
     displayedColumns = input<string[]>([]);
 
+    @ViewChildren(TableCellHostDirective) cellHosts!: QueryList<TableCellHostDirective>;
+
     constructor(private _registry: TableColumnRendererRegistryService) {
 
+    }
+
+    ngAfterViewInit(): void {
+        this.data().forEach((row, rowIndex) => {
+            this.columns().forEach((column, columnIndex) => {
+                const hostIndex = rowIndex * this.columns.length + columnIndex;
+                const host = this.cellHosts.get(hostIndex) ?? null;
+                if (host == null) {
+                    return;
+                }
+
+                const rendererType = this._registry.getRenderer(column.type);
+                if (rendererType == null) {
+                    return;
+                }
+
+                const rendererRef = host.viewContainerRef.createComponent(rendererType);
+                const field = column.field as keyof typeof row;
+                const fieldValue = row[field];
+                rendererRef.instance.render(fieldValue, row, column);
+            });
+        });
     }
 
 }
